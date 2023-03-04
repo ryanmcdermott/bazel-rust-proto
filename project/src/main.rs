bb008498f85002dc342a9583f104ad6fe1eca6b5
Ryan McDermott <ryantmcdermott@gmail.com>
2023-03-03 21:20:26 -0800

    Add text format

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,8 +1,15 @@
 use ::foo_proto_lib::Foo;
 
+use protobuf::text_format::{merge_from_str, print_to_string};
+
 fn main() {
     let mut foo = Foo::new();
     foo.name = "foo".to_owned();
 
-    println!("foo: {:?}", foo);
+    let res = print_to_string(&foo);
+    println!("foo: {:?}", res);
+
+    let mut foo2 = Foo::new();
+    merge_from_str(&mut foo2, res.as_str()).unwrap();
+    println!("foo2 name: {:?}", foo2.name);
 }
